5a032d8a64b9e4674d1d1d9a8baf567f4f25d1cb
isa <5445946+icortes@users.noreply.github.com>
2022-02-06 01:43:34 -0800

    'POST' and 'GET' users api endpoints created

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, models } from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const UserSchema = new Schema({
@@ -53,4 +53,4 @@ UserSchema.methods.checkPasword = async function (input) {
 
 const User = model('User', UserSchema);
 
-export default User;
+export default models.User || User;

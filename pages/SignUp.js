cf5b72c3f3ea0e5d1dcfc9877f5c3e5b87964c8e
isa <5445946+icortes@users.noreply.github.com>
2022-02-09 15:34:53 -0800

    added first and last name to sign up form

--- pages/SignUp.js
+++ pages/SignUp.js
@@ -1,4 +1,13 @@
-import { Container, CssBaseline } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Container,
+  CssBaseline,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 export default function SignUp() {
   const handleSubmit = (event) => {
@@ -14,7 +23,49 @@ export default function SignUp() {
   return (
     <Container component={'main'} maxWidth='xs'>
       <CssBaseline />
-      
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component={'h1'} variant='h5'>
+          Sign up
+        </Typography>
+        <Box
+          component={'form'}
+          noValidate
+          onSubmit={handleSubmit}
+          sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete='given-name'
+                name='firstName'
+                required
+                fullWidth
+                id='firstName'
+                label='First Name'
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id='lastName'
+                label='Last Name'
+                name='lastName'
+                autoComplete='family-name'
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </Container>
-  )
+  );
 }

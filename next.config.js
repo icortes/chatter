f8fa0cf05a4e89fa1ef94bf81ee71eecd61da543
isa <5445946+icortes@users.noreply.github.com>
2022-02-09 19:06:08 -0800

    added public runtime config for fatching

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ module.exports = {
   serverRuntimeConfig: {
     secret: 'SuperSecretsecret',
   },
+  publicRuntimeConfig: {
+    apiUrl:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:300/api'  //development api
+        : 'https://chatter.isaaccortes.com/api',  //product api
+  },
 };

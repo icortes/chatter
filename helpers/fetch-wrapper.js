6a11b8b4927fb801c1db53c10b9144eb9ce546ef
Isaac <isaaccortes014@gmail.com>
2022-02-27 16:37:17 -0800

    return auth header with jwt if user is logged in and request is to the api url

--- helpers/fetch-wrapper.js
+++ helpers/fetch-wrapper.js
@@ -1,5 +1,5 @@
 import getConfig from 'next/config';
-
+import { userService } from '../services/user.service';
 const { publicRuntimeConfig } = getConfig();
 
 export const fetchWrapper = {
@@ -13,4 +13,14 @@ export const fetchWrapper = {
 
 // return auth header with jwt if user is logged in and request is to the api url
 function authHeader(url) {
+  const user = userService.userValue;
+  const isLoggedIn = user && user.token;
+  const isApiUrl = url.startsWith(publicRuntimeConfig.isApiUrl);
+  if (isLoggedIn && isApiUrl) {
+    return { Authorization: `Bearer ${user.token}` };
+  } else {
+    return {};
+  }
 }
+
+

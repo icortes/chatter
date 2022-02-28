f497d9257d756d92a840bd43649d664abe79635e
Isaac <isaaccortes014@gmail.com>
2022-02-27 16:57:39 -0800

    helper function to handle response and auto log out if 401 or 403 error code is retuned from api

--- helpers/fetch-wrapper.js
+++ helpers/fetch-wrapper.js
@@ -23,4 +23,19 @@ function authHeader(url) {
   }
 }
 
+function handleResponse(response) {
+  return response.text().then(text => {
+    const data = text && JSON.parse(text);
 
+    // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
+    if (!response.ok) {
+      if ([401, 403].includes(response.status) && userService.userValue) {
+        userService.logout();
+      }
+
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+    return data;
+  })
+}
